83d083f8fb40570fe541581219aaf5504c05ed87
Lior Abitbol <lior@lpa365.com>
2026-02-04 17:32:37 -0500

    fix: s3-bucket module format

--- modules/foundation/s3-bucket/main.tf
+++ modules/foundation/s3-bucket/main.tf
@@ -37,7 +37,7 @@ resource "aws_s3_bucket_public_access_block" "this" {
   block_public_acls       = var.block_public_acls
   block_public_policy     = var.block_public_policy
   ignore_public_acls      = var.ignore_public_acls
-  restrict_public_buckets  = var.restrict_public_buckets
+  restrict_public_buckets = var.restrict_public_buckets
 }
 
 resource "aws_s3_bucket_logging" "this" {
