7c36ff0e1702cbf5b3a138f028a4afdb0894a904
Lior Abitbol <lior@lpa365.com>
2026-02-04 20:39:54 -0500

    feat: add event-driven to dev environment

--- envs/dev/main.tf
+++ envs/dev/main.tf
@@ -2,10 +2,14 @@ provider "aws" {
   region = var.aws_region
 }
 
+resource "random_id" "suffix" {
+  byte_length = 6
+}
+
 module "app_network" {
   source = "../../modules/patterns/app-network"
 
-  name           = var.name
+  name           = "${var.name}-${random_id.suffix.hex}"
   vpc_cidr_block = var.vpc_cidr_block
 
   azs                  = var.azs
@@ -14,6 +18,14 @@ module "app_network" {
 
   allowed_ingress_cidr_blocks = var.allowed_ingress_cidr_blocks
   app_ingress_ports           = var.app_ingress_ports
+
+  tags = var.tags
+}
+
+module "event_driven" {
+  source = "../../modules/patterns/event-driven"
+
+  name = "${var.name}-${random_id.suffix.hex}"
 
   tags = var.tags
 }
